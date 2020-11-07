92775a453aee5ba17442560a4ccfbdeaa9ac1792
Lanzzzzz <zlan@mun.ca>
2020-11-07 11:01:56 -0330

    Move the mouse listener of the CenterCanvas into a private class

--- src/prezoom/view/CenterCanvas.java
+++ src/prezoom/view/CenterCanvas.java
@@ -12,7 +12,8 @@ import prezoom.controller.CameraManager;
 import prezoom.Main;
 
 
-public class CenterCanvas extends JPanel implements MouseWheelListener, MouseListener, MouseMotionListener{
+public class CenterCanvas extends JPanel
+{
     int mxstart, mystart;
     private double zoomFactor = 1;
     private double prevZoomFactor = 1;
@@ -33,26 +34,32 @@ public class CenterCanvas extends JPanel implements MouseWheelListener, MouseLis
 
     // for test purpose
     public ArrayList<GObject> objects = new ArrayList<>();
+
     {
-        objects.add(new GRectangle(50, 100, Color.red, false, 1,30, 40));
-        objects.add(new GRectangle(350, 500, Color.GREEN, true, 10,30, 40));
-        objects.add(new GOval(150, 200, Color.BLUE, true,3,50,30));
+        objects.add(new GRectangle(50, 100, Color.red, false, 1, 30, 40));
+        objects.add(new GRectangle(350, 500, Color.GREEN, true, 10, 30, 40));
+        objects.add(new GOval(150, 200, Color.BLUE, true, 3, 50, 30));
     }
 
     /**
      * add Mouse Listener, Mouse Wheel Listener, and Mouse Motion Listener to this panel
      */
-    public CenterCanvas() {
-        addMouseWheelListener(this);
-        addMouseMotionListener(this);
-        addMouseListener(this);
+    public CenterCanvas()
+    {
+//        addMouseWheelListener(this);
+//        addMouseMotionListener(this);
+//        addMouseListener(this);
+        addMouseWheelListener(new MouseActionHandler());
+        addMouseMotionListener(new MouseActionHandler());
+        addMouseListener(new MouseActionHandler());
     }
 
     /**
      * move the camera to the given location
-     * @param xOffset x offset
-     * @param yOffset y offset
-     * @param zoomFactor zoom index
+     *
+     * @param xOffset        x offset
+     * @param yOffset        y offset
+     * @param zoomFactor     zoom index
      * @param prevZoomFactor previous zoom index that is used to get better effect when zooming
      */
     public void setCanvasCamera(double xOffset, double yOffset, double zoomFactor, double prevZoomFactor)
@@ -67,17 +74,20 @@ public class CenterCanvas extends JPanel implements MouseWheelListener, MouseLis
      * override the default paint method to deal with dragging, zooming by {@link CameraManager#moveCamera(Graphics2D, double, double, double, double)}.
      * check objects' attributes for the current state from {@link GAttributeManager#getCur_Attributes()},
      * skip objects whose attributes for the current state do not exist
+     *
      * @param g the graphics to paint
      */
     @Override
-    public void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g)
+    {
 
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
         AffineTransform at = new AffineTransform();
 
-        if (zoomer) {
+        if (zoomer)
+        {
 
             double xRel = MouseInfo.getPointerInfo().getLocation().getX() - getLocationOnScreen().getX();
             double yRel = MouseInfo.getPointerInfo().getLocation().getY() - getLocationOnScreen().getY();
@@ -93,27 +103,28 @@ public class CenterCanvas extends JPanel implements MouseWheelListener, MouseLis
 //            g2.transform(at);
 
             //cameraManager.setCamInfo(xOffset,yOffset,zoomFactor);
-            cameraManager.moveCamera(g2,xOffset,yOffset,zoomFactor,prevZoomFactor);
+            cameraManager.moveCamera(g2, xOffset, yOffset, zoomFactor, prevZoomFactor);
 
             //System.out.println(zoomFactor+" "+cur_off_x+" "+cur_off_y);
 
             zoomer = false;
-        }
-        else if (dragger) {
+        } else if (dragger)
+        {
             //System.out.println(xOffset + xDiff);
 //            at.translate(xOffset + xDiff, yOffset + yDiff);
 //            at.scale(zoomFactor, zoomFactor);
 //            g2.transform(at);
             //cameraManager.setCamInfo(xOffset + xDiff,yOffset + yDiff,zoomFactor);
-            cameraManager.moveCamera(g2,xOffset + xDiff,yOffset + yDiff,zoomFactor,prevZoomFactor);
+            cameraManager.moveCamera(g2, xOffset + xDiff, yOffset + yDiff, zoomFactor, prevZoomFactor);
 
-            if (released) {
+            if (released)
+            {
                 xOffset += xDiff;
                 yOffset += yDiff;
                 dragger = false;
             }
-        }
-        else {
+        } else
+        {
 //            at.translate(xOffset, yOffset);
 //            at.scale(zoomFactor, zoomFactor);
 //            g2.transform(at);
@@ -124,7 +135,8 @@ public class CenterCanvas extends JPanel implements MouseWheelListener, MouseLis
 
         //g2.setColor(Color.white);
         //g2.fillRect(0,0,2000,1000);
-        for (GObject go : objects) {
+        for (GObject go : objects)
+        {
             if (go.gAttributeManager.getCur_Attributes() != null)
                 go.draw(g2);
         }
@@ -132,124 +144,143 @@ public class CenterCanvas extends JPanel implements MouseWheelListener, MouseLis
 
     }
 
-    /**
-     * deal with zooming
-     * @param e mouse action
-     */
-    @Override
-    public void mouseWheelMoved(MouseWheelEvent e) {
-
-        zoomer = true;
-
-        //Zoom in
-        if (e.getWheelRotation() < 0) {
-            zoomFactor *= 1.1;
-            repaint();
-        }
-        //Zoom out
-        if (e.getWheelRotation() > 0) {
-            zoomFactor /= 1.1;
-            repaint();
-        }
-
-        Main.app.statusBar.setZoomText(String.format("Zoom: %3.2f %%",zoomFactor*100));
-
-    }
-
-    /**
-     * deal with dragging
-     * @param e mouse action
-     */
-    @Override
-    public void mouseDragged(MouseEvent e) {
-
-        if(SwingUtilities.isRightMouseButton(e))
+    private class MouseActionHandler implements MouseWheelListener, MouseMotionListener, MouseListener
+    {
+        /**
+         * deal with zooming
+         *
+         * @param e mouse action
+         */
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e)
         {
-            Point curPoint = e.getLocationOnScreen();
-            xDiff = curPoint.x - startPoint.x;
-            yDiff = curPoint.y - startPoint.y;
 
-            dragger = true;
-            repaint();
+            zoomer = true;
+
+            //Zoom in
+            if (e.getWheelRotation() < 0)
+            {
+                zoomFactor *= 1.1;
+                repaint();
+            }
+            //Zoom out
+            if (e.getWheelRotation() > 0)
+            {
+                zoomFactor /= 1.1;
+                repaint();
+            }
+
+            Main.app.statusBar.setZoomText(String.format("Zoom: %3.2f %%", zoomFactor * 100));
+
         }
-        else if(selectedObj != null){
-            int mx=e.getX();
-            int my=e.getY();
-            //System.out.println(mx-mxstart);
+
+        /**
+         * deal with dragging
+         *
+         * @param e mouse action
+         */
+        @Override
+        public void mouseDragged(MouseEvent e)
+        {
+
+            if (SwingUtilities.isRightMouseButton(e))
+            {
+                Point curPoint = e.getLocationOnScreen();
+                xDiff = curPoint.x - startPoint.x;
+                yDiff = curPoint.y - startPoint.y;
+
+                dragger = true;
+                repaint();
+            } else if (selectedObj != null)
+            {
+                int mx = e.getX();
+                int my = e.getY();
+                //System.out.println(mx-mxstart);
 
 //            double z = 1;
 //            if (zoomFactor<1)
 //                z = zoomFactor;
 
-            selectedObj.setX(selectedObj.getX()+ /*(int)*/((mx-mxstart)/zoomFactor));
-            selectedObj.setY(selectedObj.getY()+ /*(int)*/((my-mystart)/zoomFactor));
-            //selectedObj.setX(mx);
-            //selectedObj.setY(my);
-            mxstart=mx;
-            mystart=my;
-            repaint();
+                selectedObj.setX(selectedObj.getX() + /*(int)*/((mx - mxstart) / zoomFactor));
+                selectedObj.setY(selectedObj.getY() + /*(int)*/((my - mystart) / zoomFactor));
+                //selectedObj.setX(mx);
+                //selectedObj.setY(my);
+                mxstart = mx;
+                mystart = my;
+                repaint();
+
+            }
+
 
         }
 
+        @Override
+        public void mouseMoved(MouseEvent e)
+        {
+            Main.app.statusBar.setStatusText(String.format("Moving at [%d,%d]", e.getX(), e.getY()));
+        }
 
-    }
+        @Override
+        public void mouseClicked(MouseEvent e)
+        {
+            mxstart = e.getX();
+            mystart = e.getY();
+        }
 
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        Main.app.statusBar.setStatusText(String.format("Moving at [%d,%d]",e.getX(),e.getY()));
-    }
+        /**
+         * when pressed check whether an object is selected
+         *
+         * @param e mouse action
+         */
+        @Override
+        public void mousePressed(MouseEvent e)
+        {
+            released = false;
+            startPoint = MouseInfo.getPointerInfo().getLocation();
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        mxstart = e.getX();
-        mystart = e.getY();
-    }
+            mxstart = e.getX();
+            mystart = e.getY();
+            //System.out.println(mxstart);
 
-    /**
-     * when pressed check whether an object is selected
-     * @param e mouse action
-     */
-    @Override
-    public void mousePressed(MouseEvent e) {
-        released = false;
-        startPoint = MouseInfo.getPointerInfo().getLocation();
-
-        mxstart = e.getX();
-        mystart = e.getY();
-        //System.out.println(mxstart);
-
-        //mxstart -= o_X;
-        //mystart -= o_Y;
-        double mx=(e.getX()-cameraManager.cur_CamInfo.cam_x_offset)/zoomFactor;
-        double my=(e.getY()-cameraManager.cur_CamInfo.cam_y_offset)/zoomFactor;
+            //mxstart -= o_X;
+            //mystart -= o_Y;
+            double mx = (e.getX() - cameraManager.cur_CamInfo.cam_x_offset) / zoomFactor;
+            double my = (e.getY() - cameraManager.cur_CamInfo.cam_y_offset) / zoomFactor;
 //        int mx=e.getX();
 //        int my=e.getY();
-        for (GObject go : objects)  {
-            if (go.inShape(mx, my)) {
-                selectedObj = go;
+            for (GObject go : objects)
+            {
+                if (go.inShape(mx, my))
+                {
+                    selectedObj = go;
+                }
             }
+            //repaint();
+            //mxstart=mx;
+            //mystart=my;
         }
-        //repaint();
-        //mxstart=mx;
-        //mystart=my;
+
+        @Override
+        public void mouseReleased(MouseEvent e)
+        {
+            released = true;
+            selectedObj = null;
+            repaint();
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e)
+        {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e)
+        {
+
+        }
+
     }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        released = true;
-        selectedObj = null;
-        repaint();
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
 
 }
+
